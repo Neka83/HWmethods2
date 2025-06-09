b75194d044f10254fe3b921fecd174e1bbcd7c19
Neka83 <n_neike@mail.ru>
2025-06-09 15:39:13 +0500

    HWmethods#2

--- src/Main.java
+++ src/Main.java
@@ -43,39 +43,35 @@ public class Main {
     public static void recommendAppVersion(int osType, int deviceYear) {
         int currentYear = LocalDate.now().getYear();
 
-        switch  (osType) {
-            case 0:
-                if (deviceYear < currentYear){
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        if (osType == 0) {
+            if (deviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            }
+        } else if (osType == 1) {
+            if (deviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
-            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
-                break;
-            case 1:
-                if (deviceYear < currentYear){
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                } else {
-                    System.out.println("Установите обычную версию приложения для Android по ссылке");
-                }
-                break;
-            default:
-               break;
-        }
-
 
     }
 
-    public static int calculateDeliveryDays(int distance) {
-        if (distance <= 20) {
-            return 1;
-        } else if (distance <= 60) {
-            return 2;
-        } else if (distance <= 100) {
-            return 3;
-        } else {
-            return -1;
-        }
+
+}
+
+public static int calculateDeliveryDays(int distance) {
+    if (distance <= 20) {
+        return 1;
+    } else if (distance <= 60) {
+        return 2;
+    } else if (distance <= 100) {
+        return 3;
+    } else {
+        return -1;
     }
+}
 
 
 }
